9c8ce826df677a7afd7c1ec53beb37a0ea97aebd
= <peterhjhan@gmail.com>
2018-11-06 22:56:54 +0000

    question db completed

--- db/math_question.rb
+++ db/math_question.rb
@@ -18,46 +18,88 @@ require 'active_support/all'
   },
   {
     id: 4,
-    question: "some-text",
-    answer: "" 
+    question: "How many seconds are there in 1 hour?",
+    answer: "3600" 
   },
   {
     id: 5,
-    question: "some-text",
-    answer: "" 
+    question: "What is PI to the 5th decimal (i.e. 3.xxxxx",
+    answer: "3.14159" 
   },
 
   {
-    id: 1,
-    question: "some-text",
-    answer: "" 
+    id: 6,
+    question: "What is 12 plus 46 plus 19 minus 5",
+    answer: "72" 
   },
   {
-    id: 1,
-    question: "some-text",
-    answer: "" 
+    id: 7,
+    question: "How many 10s are in 1200?",
+    answer: "120" 
   },
   {
-    id: 1,
-    question: "some-text",
-    answer: "" 
+    id: 8,
+    question: "A pizza has 20 slices, and there are 4 people. How many slices should each person get?",
+    answer: "5" 
   },
   {
-    id: 1,
-    question: "some-text",
-    answer: "" 
+    id: 9,
+    question: "What is 132 divided by 11?",
+    answer: "12" 
   },
   {
-    id: 1,
-    question: "some-text",
-    answer: "" 
+    id: 10,
+    question: "What is 1 plus 1",
+    answer: "2" 
   },
   {
-    id: 1,
-    question: "some-text",
-    answer: "" 
+    id: 11,
+    question: "How many eggs are in a dozen?",
+    answer: "12" 
   },
-  
-
-
+  {
+    id: 12,
+    question: "What is the volume of the cube with 3cm for each sides?",
+    answer: "27" 
+  },
+  {
+    id: 13,
+    question: "What is 2 to the power of 6?",
+    answer: "64" 
+  },
+  {
+    id: 14,
+    question: "What is the square root of 9?",
+    answer: "3" 
+  },
+  {
+    id: 15,
+    question: "What is the sqaure root of 25?",
+    answer: "5" 
+  },
+  {
+    id: 16,
+    question: "What is the square root of 121?",
+    answer: "11" 
+  },
+  {
+    id: 17,
+    question: "What is 3 plus 3 times 3?",
+    answer: "12" 
+  },
+  {
+    id: 18,
+    question: "What is the area of a triangle with height of 10 and base of 5?",
+    answer: "25" 
+  },
+  {
+    id: 19,
+    question: "What is 8 times 6?",
+    answer: "48" 
+  },
+  {
+    id: 20,
+    question: "What is 9 times 7?",
+    answer: "63" 
+  }
 ]
