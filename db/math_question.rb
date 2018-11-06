79ac84f15b5cff432b1d5de8532cb770020d6788
= <peterhjhan@gmail.com>
2018-11-06 21:34:42 +0000

    initalize folder structure

---
+++ db/math_question.rb
@@ -0,0 +1,63 @@
+require 'active_support/all'
+
+@math_questions = [
+  {
+    id: 1,
+    question: "What is 2 time 3",
+    answer: "6" 
+  },
+  {
+    id: 2,
+    question: "What is 12 times 12 ",
+    answer: "144" 
+  },
+  {
+    id: 3,
+    question: "I have 6 apples and 2 bananas. If I eat 2 apples, how many bananas are left.",
+    answer: "2" 
+  },
+  {
+    id: 4,
+    question: "some-text",
+    answer: "" 
+  },
+  {
+    id: 5,
+    question: "some-text",
+    answer: "" 
+  },
+
+  {
+    id: 1,
+    question: "some-text",
+    answer: "" 
+  },
+  {
+    id: 1,
+    question: "some-text",
+    answer: "" 
+  },
+  {
+    id: 1,
+    question: "some-text",
+    answer: "" 
+  },
+  {
+    id: 1,
+    question: "some-text",
+    answer: "" 
+  },
+  {
+    id: 1,
+    question: "some-text",
+    answer: "" 
+  },
+  {
+    id: 1,
+    question: "some-text",
+    answer: "" 
+  },
+  
+
+
+]
